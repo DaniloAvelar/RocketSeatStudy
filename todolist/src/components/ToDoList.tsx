ec3ae8714c9a853f58ee741d6fa5102a5b084e18
Danilo Avelar <da.avelar@hotmail.com>
2024-12-20 11:10:29 -0300

    TDL0004: Cria a lista de ToDo com div e elemntos Radio e icone lixeira

--- todolist/src/components/ToDoList.tsx
+++ todolist/src/components/ToDoList.tsx
@@ -1,19 +1,65 @@
 import styles from "./ToDoList.module.css";
 import iconWithoutTask from "../assets/Clipboard.png";
+import { useState } from "react";
+import { FaTrash } from 'react-icons/fa';
 
 export function TodoList() {
+
+    const [createdTask, setCreatedTask] = useState(1);
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+        console.log(checked);
+
+    }
+
     return (
         <div className={styles.listContent}>
             <header className={styles.headerList}>
-                <span className={styles.created}>Tarefas criadas: <span className={styles.createdResult}>4</span></span>
+                <span className={styles.created}>Tarefas criadas: <span className={styles.createdResult}>{createdTask}</span></span>
                 <span className={styles.completed}>Concluídas: <span className={styles.completedResult}>12</span></span>
             </header>
-            <div className={styles.divider}></div>
-            <div className={styles.task}>
-                <div className={styles.emptTask}>
-                    <img src={iconWithoutTask} alt="" />
-                    <p style={{ fontWeight: 'bold', fontSize:16 }}>Você ainda não tem tarefas cadastradas</p>
-                    <p>Crie tarefas e organize seus itens a fazer</p>
+
+            {createdTask === 0 && (
+                <div>
+                    <div className={styles.divider}></div>
+                    <div className={styles.task}>
+                        <div className={styles.emptTask}>
+                            <img src={iconWithoutTask} alt="" />
+                            <p style={{ fontWeight: 'bold', fontSize: 16 }}>Você ainda não tem tarefas cadastradas</p>
+                            <p>Crie tarefas e organize seus itens a fazer</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {/* Lista de tarrefas criadas */}
+            <div className={
+                checked
+                ? styles.taskToDoChecked
+                : styles.taskToDo
+                }
+            >
+                <div>
+                    <input
+                        type="checkbox"
+                        id="todo"
+                        checked={checked}
+                        onChange={handleChange}
+                        className={styles.inputRadio}
+                    />
+                </div>
+                <span
+                    className={
+                        checked 
+                        ? styles.checkedSpan 
+                        : styles.uncheckedSpan
+                    }
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi recusandae, consectetur aspernatur, eum ullam facilis cumque hic ut repellendus iure, similique ab eveniet dolor animi natus. Nobis facere tenetur tempore!
+                </span>
+                <div className={styles.iconsTrash}>
+                    <FaTrash className={styles.trashIcon} />
                 </div>
             </div>
         </div>
